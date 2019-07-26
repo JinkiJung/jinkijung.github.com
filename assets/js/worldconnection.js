1ac85dca7e9bc14952e1db4facd0d2b28597cb49
Jinki Jung <your.jinki.jung@gmail.com>
2019-07-26 17:14:01 +0200

    bug fixed

--- assets/js/worldconnection.js
+++ assets/js/worldconnection.js
@@ -8,30 +8,36 @@ var mymap = L.map('my-custom-map').setView([33.302986, 40.352783], 1);
 		id: 'mapbox.streets'
 	}).addTo(mymap);
 
-	L.marker([55.652599, 12.544167]).addTo(mymap)
-		.bindPopup("I am here!").openPopup();
+  var jinkiCircleIcon = {
+      radius: 500,
+      fillColor: "#ff7800",
+      color: "red",
+      weight: 3,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
 
-L.marker([53.148790, 8.200178]).addTo(mymap)
+  var shipCircleIcon = {
+      radius: 500,
+      fillColor: "#ff7800",
+      color: "green",
+      weight: 3,
+      opacity: 1,
+      fillOpacity: 0.8
+  };
+
+	L.circle([55.652599, 12.544167], jinkiCircleIcon).addTo(mymap)
+		.bindPopup("I am here!");
+
+L.circle([53.148790, 8.200178], shipCircleIcon).addTo(mymap)
   .bindPopup("University of Oldenburg");
-  L.marker([31.274758, 120.738244]).addTo(mymap)
+  L.circle([31.274758, 120.738244], shipCircleIcon).addTo(mymap)
 		.bindPopup("Xi'an Jiaotong-Liverpool University");
-    L.marker([36.102804, 129.388426]).addTo(mymap)
+    L.circle([36.102804, 129.388426], shipCircleIcon).addTo(mymap)
   		.bindPopup("Handong Global University");
-      L.marker([28.601580, -81.200693]).addTo(mymap)
+      L.circle([28.601580, -81.200693], shipCircleIcon).addTo(mymap)
         .bindPopup("University of Central Florida");
-        L.marker([36.398072, 127.363214]).addTo(mymap)
+        L.circle([36.398072, 127.363214], shipCircleIcon).addTo(mymap)
           .bindPopup("Korea Research Institute of Ships and Ocean Engineering");
-          L.marker([36.368721, 127.367677]).addTo(mymap)
+          L.circle([36.368721, 127.367677], shipCircleIcon).addTo(mymap)
             .bindPopup("KAIST");
-
-
-	var popup = L.popup();
-
-	function onMapClick(e) {
-		popup
-			.setLatLng(e.latlng)
-			.setContent("You clicked the map at " + e.latlng.toString())
-			.openOn(mymap);
-	}
-
-	mymap.on('click', onMapClick);
